703a5f92819c1a86d4190553f61dd0e1147668e6
mokhelek <mkatleho191@gmail.com>
2023-11-30 13:27:02 +0200

    setup and updated client side

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
-axios.get("/api/vehicles").then((result) => {
-    console.log(result.data)
+const vehicles = axios.get("/api/vehicles").then((result) => {
+    let templatePlaceHolder = document.querySelector(".vehicles");
+    let template = document.querySelector(".vehicles-template").innerHTML;
+    let compiledTemplate = Handlebars.compile(template);
+    let vehicles = result.data.data
+    templatePlaceHolder.innerHTML = compiledTemplate({ vehicles });
 });
+
+
+function submitForm() {
+    const description = document.getElementById('description').value;
+    const regNumber = document.getElementById('reg-num').value;
+
+    const formData = {
+        description: description,
+        regNumber: regNumber
+    };
+    console.log(formData)
+
+    axios.post("/api/vehicle", formData).then(()=>{
+        alert("Successfully added")
+    })
+
+}
